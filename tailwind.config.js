20a65a60f534f546a4bd6911070138512a861d42
Thomas Eduardo <developer.thomas@outlook.com.br>
2024-12-01 16:30:41 -0300

    corrigindo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,23 @@
+// tailwind.config.mjs
+import withMT from "@material-tailwind/react/utils/withMT";
+import { nextui } from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = withMT({
+  // Arquivos onde o Tailwind CSS irá procurar classes
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html", // Se você tiver elementos diretamente no HTML
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  // Configurações do tema
   theme: {
-    extend: {
-      colors: {
-        'primary': '#81ff94', // Cor principal do seu projeto
-        'secondary': '#1DA65D', // Cor de destaque
-        'black': '#000000', // Preto puro
-        'white': '#ffffff', // Branco puro
-        'gray-light': '#f5f5f5', // Cinza claro (para fundo)
-        'gray-dark': '#333333', // Cinza escuro (para textos)
-      },
-      fontFamily: {
-        'sans': ['Inter', 'Arial', 'sans-serif'], // Fonte padrão
-      },
-      spacing: {
-        '128': '32rem', // Adiciona um novo valor de espaçamento (ex: 32rem)
-        '144': '36rem', // Outro valor de espaçamento
-      },
-      boxShadow: {
-        'custom': '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)',
-      },
-      screens: {
-        'xs': '475px', // Para telas pequenas
-      },
-    },
+    extend: {}, // Extensões personalizadas podem ser adicionadas aqui
   },
-  plugins: [
-    require('@tailwindcss/forms'), // Para um estilo melhor em formulários
-    require('@tailwindcss/typography'), // Para textos bonitos e fáceis de ler
-  ],
-}
+  // Habilita o modo escuro através da classe "dark"
+  darkMode: "class",
+  // Plugins utilizados no Tailwind CSS
+  plugins: [nextui()],
+});
+
+export default config;
